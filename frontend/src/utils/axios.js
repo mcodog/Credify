452649c6a29c0f83da7f339907d115d28aa9ace2
mcodog <156042950+mcodog@users.noreply.github.com>
2025-06-08 18:12:30 +0800

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://credify-wy50.onrender.com/api",
   timeout: 20000,
   headers: {
     "Content-Type": "application/json",
